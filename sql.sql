84a3d2f4d522966d53d0ec09b069d64649494378
Lukasz Bednorz <lbednorz.sag@gmail.com>
2020-10-03 19:33:55 +0200

    first JOOQ select

--- sql.sql
+++ sql.sql
@@ -3,3 +3,5 @@ CREATE TABLE tanks (
     text_id VARCHAR(30) UNIQUE DEFAULT NULL,
     name VARCHAR (50) DEFAULT NULL
 );
+
+INSERT INTO tanks (text_id, name) VALUES ('tank_1', 'Tank One');
--- src/main/java/pl/snipersoft/wwtanks/DataManager.scala
+++ src/main/java/pl/snipersoft/wwtanks/DataManager.scala
@@ -2,8 +2,9 @@ package pl.snipersoft.wwtanks
 
 import java.sql.DriverManager
 
-import org.jooq.SQLDialect
+import org.jooq.{Record, Result, SQLDialect}
 import org.jooq.impl.DSL
+import pl.snipersoft.wwtanks.jooq.Tables
 
 class DataManager(private val configuration: DatabaseConfiguration) {
   def start(): Unit = {
@@ -17,6 +18,14 @@ object Database {
   def initDatabase() {
     val configuration = DatabaseConfiguration(url = "jdbc:postgresql://localhost:5432/wwtanks", username = "postgres", password = "admin")
     val connection = DriverManager.getConnection(configuration.url, configuration.username, configuration.password)
-    val dslContext = DSL.using(SQLDialect.POSTGRES)
+    val dslContext = DSL.using(connection, SQLDialect.POSTGRES)
+    val result: Result[Record] = dslContext.select().from(Tables.TANKS).fetch()
+    println(result)
+    val dtos = result.map(it => Tank(id = it.getValue(Tables.TANKS.ID), textId = it.getValue(Tables.TANKS.TEXT_ID),
+      name = it.getValue(Tables.TANKS.NAME)))
+    println(dtos)
+    connection.close()
   }
 }
+
+case class Tank(id: Int, textId: String, name: String)
