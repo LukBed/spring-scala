a3e49bb618d2485f7fd03a9909658f6c00813a73
Lukasz Bednorz <lbednorz.sag@gmail.com>
2020-10-03 19:48:16 +0200

    no jooq

--- src/main/java/pl/snipersoft/wwtanks/WwtanksApplication.java
+++ src/main/java/pl/snipersoft/wwtanks/WwtanksApplication.java
@@ -8,7 +8,7 @@ public class WwtanksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WwtanksApplication.class, args);
-		Database.initDatabase();
+//		Database.initDatabase();
 	}
 
 }
